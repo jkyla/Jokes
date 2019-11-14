871bcf0eab931d9c2917db823568246663ba08c0
Kyla Johnson <johnsonkyla18@yahoo.com>
2019-11-14 14:17:22 -0600

    Initial Commit

---
+++ DadJokes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DadJokes
+//
+//  Created by Kyla Johnson  on 11/14/19.
+//  Copyright © 2019 Kyla Johnson . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
